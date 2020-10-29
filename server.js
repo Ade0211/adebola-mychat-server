6e0bba5883c8d9274d6f63e2a902d913788a0f98
Glitch (distinct-dark-ixia) <none>
2020-10-29 20:21:50 +0000

    🌮🐘 Checkpoint ./server.js:23950603/3

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require('cors')
 const app = express();
 const bodyParser = require("body-parser")
-// app.use(bodyParser.json())
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors())
 let welcomeMessage = {
